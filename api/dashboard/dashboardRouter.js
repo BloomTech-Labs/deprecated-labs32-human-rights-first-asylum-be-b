b93ddf80dfc77dfd281eee0e9cf5546d597458b0
David Gold <davidgold@hey.com>
2020-12-14 10:51:03 -0500

    added multer and have a basic api set up to accept images

--- api/dashboard/dashboardRouter.js
+++ api/dashboard/dashboardRouter.js
@@ -2,12 +2,28 @@ const express = require('express');
 const authRequired = require('../middleware/authRequired');
 const Dashboard = require('./dashboardModel');
 const router = express.Router();
+const multer = require('multer');
+const fs = require('fs-extra');
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, callback) => {
+      console.log(file);
+      let userId = req.body.user_id;
+      let path = `../../uploads/${userId}`;
+      fs.mkdirsSync(path);
+      callback(null, path);
+    },
+    filename: (req, file, callback) => {
+      callback(null, file.originalname);
+    },
+  }),
+});
 
 // To insert Swagger docs here
 
 // TEST PURPOSES
-router.get('/', authRequired, function (req, res) {
-  Dashboard.findAll()
+router.get('/', function (req, res) {
+  Dashboard.getAll()
     .then((info) => {
       res.status(200).json(info);
     })
@@ -15,6 +31,11 @@ router.get('/', authRequired, function (req, res) {
       console.log(err);
       res.status(500).json({ message: err.message });
     });
+});
+
+// Upload
+router.post('/upload', upload.single('upload'), (req, res) => {
+  res.status(200).send();
 });
 
 // REAL
