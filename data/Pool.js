a2503937d304e6708c7a2f7d62497802788aea92
aurelio <aurelioarcabascio@gmail.com>
2020-12-16 17:39:14 -0500

    Added test

--- data/Pool.js
+++ data/Pool.js
@@ -1,11 +1,22 @@
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//   user: 'docker',
+//   host: '127.0.0.1',
+//   database: 'api-dev',
+//   password: 'docker',
+//   port: 5432,
+// });
+
+console.log('TEST 1');
+console.log(process.env.DATABASE_URL);
+console.log('TEST 2');
+
 const pool = new Pool({
-  user: 'docker',
-  host: '127.0.0.1',
-  database: 'api-dev',
-  password: 'docker',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
