09a55f20f3fdba5370d6d3714b9c76ff255f315f
purplehays09 <purplehays527@gmail.com>
2021-01-19 16:27:15 -0700

    added the seeds file for the main categories. There is still an issue with the config file but we can fix that after

---
+++ seeds/01-main_categories.js
@@ -0,0 +1,25 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('main_categories').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('main_categories').insert([
+        {
+          'main_category_name': 'Protected Ground',
+        },
+        {
+          'main_category_name': 'Particular Social Group',
+        },
+        {
+          'main_category_name': 'Precedential Decision',
+        },
+        {
+          'main_category_name': 'Policies',
+        },
+        {
+          'main_category_name': 'Bars to Asylum',
+        },
+      ]);
+    });
+};
