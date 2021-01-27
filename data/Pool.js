ffed8dbdff3da3a6def9f53594ab25777af8f2e0
unknown <SeanDShadle@gmail.com>
2021-01-27 17:10:04 -0600

    changed pool to production

--- data/Pool.js
+++ data/Pool.js
@@ -1,18 +1,18 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: 'docker',
-  host: '127.0.0.1',
-  database: 'api-dev',
-  password: 'docker',
-  port: 5432,
-});
-
 // const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
+//   user: 'docker',
+//   host: '127.0.0.1',
+//   database: 'api-dev',
+//   password: 'docker',
+//   port: 5432,
 // });
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = pool;
